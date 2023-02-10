101be2c74e6493844b62982565846f729d1bcdd0
cassiocunha <cassio1706@gmail.com>
2023-02-10 12:04:40 -0600

    Add unit tests

---
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -0,0 +1,70 @@
+package br.ce.wcaquino.taskbackend.controller;
+
+import br.ce.wcaquino.taskbackend.model.Task;
+import br.ce.wcaquino.taskbackend.repo.TaskRepo;
+import br.ce.wcaquino.taskbackend.utils.ValidationException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDate;
+
+public class TaskControllerTest {
+
+    @Mock
+    private TaskRepo taskRepo;
+    @InjectMocks
+    private TaskController controller;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaSemDescricao() {
+        try {
+            Task task = new Task();
+            task.setDueDate(LocalDate.now());
+            controller.save(task);
+        } catch (ValidationException e) {
+            Assert.assertEquals("Fill the task description", e.getMessage());
+        }
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaSemData() {
+        try {
+            Task task = new Task();
+            task.setTask("Something");
+            controller.save(task);
+        } catch (ValidationException e) {
+            Assert.assertEquals("Fill the due date", e.getMessage());
+        }
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaComDataPassada() {
+        try {
+            Task task = new Task();
+            task.setTask("Something");
+            task.setDueDate(LocalDate.of(2010, 01, 01));
+            controller.save(task);
+        } catch (ValidationException e) {
+            Assert.assertEquals("Due date must not be in past", e.getMessage());
+        }
+    }
+
+    @Test
+    public void deveSalvarTarefaComSucesso() throws ValidationException {
+        Task task = new Task();
+        task.setTask("Something");
+        task.setDueDate(LocalDate.now());
+        controller.save(task);
+        Mockito.verify(taskRepo).save(task);
+    }
+}
